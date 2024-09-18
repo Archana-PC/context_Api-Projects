3cb8e5fd46b33a5bc2c0717ae9d7ed88a4bdcc28
Archana-PC <archanamikki1998@gmail.com>
2024-09-18 19:21:41 +0530

    add context apt based project

---
+++ 01ContextApi/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Profile from './components/Profile'
+import Login from './components/Login';
+import UserContextProvider from './context/UserContextProvider';
+
+function App() {
+  return (
+    <UserContextProvider>
+    <h1>React and chai</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
+  )
+}
+
+
+export default App
+
